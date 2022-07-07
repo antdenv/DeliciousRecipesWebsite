372a78418893b5c52c53032541ab8c900ebbe88b
antdenv <79360639+antdenv@users.noreply.github.com>
2022-07-07 18:21:19 +0300

    Update Veggie.jsx

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -16,7 +16,7 @@ function Veggie() {
         if (check) {
             setVeggie(JSON.parse(check));
         } else {
-            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`);
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=07ef8539b7824e59bba487331408ef76&number=9&tags=vegetarian`);
             const data = await api.json();
             localStorage.setItem('veggie', JSON.stringify(data.recipes));
             setVeggie(data.recipes);
